8793e8fb9e92965cf430ff26a1899e026d12446b
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-06 00:11:17 +0300

    добавлены тесты для проверки методов треугольника

---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTests {
+    @Test
+    void canCalculateArea() {
+        var t = new Triangle(6.0, 8.0, 10.0);
+        double result = t.area();
+        Assertions.assertEquals(24.0, result);
+    };
+
+    @Test
+    void canCalculatePerimeter(){
+        var t = new Triangle(6.0, 8.0, 10.0);
+        double result = t.perimeter();
+        Assertions.assertEquals(24.0, result);
+    }
+}
