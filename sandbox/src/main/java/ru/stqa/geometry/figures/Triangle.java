d900a7ba1bcb31df083359e775e20d49a97eed4a
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-05 23:28:43 +0300

    создан класс Triangle

---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -0,0 +1,13 @@
+package ru.stqa.geometry.figures;
+
+public class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+}
