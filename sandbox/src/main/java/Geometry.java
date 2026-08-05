971fa157fb2444f4472af40efb47062af6fe55ba
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-05 14:31:58 +0300

    добавлен метод printRectangleArea

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -1,6 +1,15 @@
 public class Geometry {
     public static void main(String[] args) {
         printSquareArea(6);
+        printRectangleArea(3.0, 5.0);
+    }
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прямоуголника со сторонами " + a + " и " + b + " = " + rectangleArea(a, b));
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return a * b;
     }
 
     static void printSquareArea(double side) {
