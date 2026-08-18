411babef93e9deafac58638f63d04b4bbeb8d8a1
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-18 14:49:13 +0300

    рефакторинг теста canCreateContact

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,25 +1,14 @@
 package tests;
 
+import model.ContactData;
 import org.junit.jupiter.api.Test;
 
-public class ContactCreationTests extends TestBase{
+public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void canCreateContact() {
-    driver.findElement(By.linkText("add new")).click();
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).sendKeys("qwe");
-    driver.findElement(By.name("middlename")).click();
-    driver.findElement(By.name("middlename")).sendKeys("qwe");
-    driver.findElement(By.name("lastname")).click();
-    driver.findElement(By.name("lastname")).sendKeys("qwe");
-    driver.findElement(By.name("address")).click();
-    driver.findElement(By.name("address")).sendKeys("moscow");
-    driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).sendKeys("+74951111111");
-    driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).sendKeys("q@m.ru");
-    driver.findElement(By.cssSelector("input:nth-child(71)")).click();
-    driver.findElement(By.linkText("home page")).click();
-  }
+    @Test
+    public void canCreateContact() {
+        app.contacts().createContact(
+                new ContactData("Ivan", "Ivanych", "Ivanov", "Москва", "+7(495)577-05-13", "q@m.ru"));
+    }
+
 }
