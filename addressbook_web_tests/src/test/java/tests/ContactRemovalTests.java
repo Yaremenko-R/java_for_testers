28dea7beb9b9d20332a23f247d80f1f8f7f3a016
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-22 23:22:07 +0300

    добавлен контроль удаления контактов через UI

--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -1,16 +1,20 @@
 package tests;
 
 import model.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ContactRemovalTests extends TestBase {
 
     @Test
     public void canRemoveContact() {
-        if (app.contacts().isContactPresent()) {
+        if (app.contacts().getCount() == 0) {
             app.contacts().createContact(
                     new ContactData("Ivan", "Ivanych", "Ivanov", "Москва", "+7(495)577-05-13", "q@m.ru"));
         }
+        int contactCount = app.contacts().getCount();
         app.contacts().removeContact();
+        int newContactCount = app.contacts().getCount();
+        Assertions.assertEquals(contactCount - 1, newContactCount);
     }
 }
