4a04e88e905b44d17e55f3cd4e925708d3f943ac
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-23 23:33:51 +0300

    в констрeктор GroupData добавлено свойство id

--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -9,7 +9,7 @@ public class GroupRemovalTests extends TestBase {
     @Test
     public void canRemoveGroup() {
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+            app.groups().createGroup(new GroupData("", "group name", "group header", "group footer"));
         }
         int groupCount = app.groups().getCount();
         app.groups().removeGroup();
@@ -20,7 +20,7 @@ public class GroupRemovalTests extends TestBase {
     @Test
     public void canRemoveAllGroupsAtOnce(){
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+            app.groups().createGroup(new GroupData("", "group name", "group header", "group footer"));
         }
         app.groups().removeAllGroups();
         Assertions.assertEquals(0, app.groups().getCount());
