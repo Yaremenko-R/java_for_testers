3a34bb3acd533a2b36a28f62b1b237859b8a6f2d
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-23 19:22:52 +0300

    параметризирован тест на массовое создание контактов

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -2,28 +2,41 @@ package tests;
 
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void canCreateContact() {
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>();
+        for (var firstname : List.of("", "contact name")) {
+            for (var middlename : List.of("", "contact header")) {
+                for (var lastname : List.of("", "contact footer")) {
+                    result.add(new ContactData(firstname, middlename, lastname, "Москва", "+7(495)577-05-13", "q@m.ru"));
+                }
+            }
+        }
+        for (int i = 0; i < 7; i++) {
+            result.add(new ContactData(randomString(i * 5),
+                    randomString(i * 5),
+                    randomString(i * 5),
+                    randomString(i * 5),
+                    "+7(495)577-05-13",
+                    randomString(i * 5) + "@m.ru"));
+        }
+        return result;
+    }
+
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void canCreateMultipleContacts(ContactData contact) {
         int contactCount = app.contacts().getCount();
-        app.contacts().createContact(
-                new ContactData("Ivan", "Ivanych", "Ivanov", "Москва", "+7(495)577-05-13", "q@m.ru"));
+        app.contacts().createContact(contact);
         int newContactCount = app.contacts().getCount();
         Assertions.assertEquals(contactCount + 1, newContactCount);
     }
 
-    @Test
-    public void canCreateMultipleContacts() {
-        int n = 5;
-        int contactCount = app.contacts().getCount();
-        for (int i = 0; i < n; i++) {
-            app.contacts().createContact(
-                    new ContactData(randomString(i * 3), "Ivanych", "Ivanov", "Москва", "+7(495)577-05-13", "q@m.ru"));
-        }
-        int newContactCount = app.contacts().getCount();
-        Assertions.assertEquals(contactCount + n, newContactCount);
-    }
 }
