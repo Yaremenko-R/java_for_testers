b06020639ac41d20a38435493da7b111ac7e1a1a
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-23 18:14:47 +0300

    имена групп создаются теперь при помощи groupNameProvider

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -4,17 +4,19 @@ import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"group Name", "group Rame", "group Pame"})
-    public void canCreateGroup(String name) {
-        int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
-        int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + 1, newGroupCount);
+    public static List<String> groupNameProvider() {
+        var result = new ArrayList<String>(List.of("group Name", "group Rame", "group Pame"));
+        for (int i = 0; i < 7; i++) {
+            result.add(randomString(i * 5));
+        }
+        return result;
     }
 
     @Test
@@ -27,14 +29,12 @@ public class GroupCreationTests extends TestBase {
         app.groups().createGroup(new GroupData().withName("some name"));
     }
 
-    @Test
-    public void canCreateMultipleGroups() {
-        int n = 5;
+    @ParameterizedTest
+    @MethodSource("groupNameProvider")
+    public void canCreateMultipleGroups(String name) {
         int groupCount = app.groups().getCount();
-        for (int i = 0; i < n; i++) {
-            app.groups().createGroup(new GroupData(randomString(i * 3), "group header", "group footer"));
-        }
+        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
         int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + n, newGroupCount);
+        Assertions.assertEquals(groupCount + 1, newGroupCount);
     }
 }
