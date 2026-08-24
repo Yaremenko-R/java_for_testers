6f475dc03c44441d94e32799af89d471a24093a8
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-24 19:59:39 +0300

    доработан провайдер тестовых данных

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -15,17 +15,24 @@ public class ContactCreationTests extends TestBase {
         for (var firstname : List.of("", "contact name")) {
             for (var middlename : List.of("", "contact header")) {
                 for (var lastname : List.of("", "contact footer")) {
-                    result.add(new ContactData(firstname, middlename, lastname, "Москва", "+7(495)577-05-13", "q@m.ru"));
+                    result.add(new ContactData()
+                            .withFirstname(firstname)
+                            .withMiddlename(middlename)
+                            .withLastname(lastname)
+                            .withAddress("Москва")
+                            .withMobile("+7(495)577-05-13")
+                            .withEmail("q@m.ru"));
                 }
             }
         }
         for (int i = 0; i < 7; i++) {
-            result.add(new ContactData(randomString(i * 5),
-                    randomString(i * 5),
-                    randomString(i * 5),
-                    randomString(i * 5),
-                    "+7(495)577-05-13",
-                    randomString(i * 5) + "@m.ru"));
+            result.add(new ContactData()
+                    .withFirstname(randomString(i * 5))
+                    .withMiddlename(randomString(i * 5))
+                    .withLastname(randomString(i * 5))
+                    .withAddress(randomString(i * 5))
+                    .withMobile("+7(495)577-05-13")
+                    .withEmail(randomString(i * 5) + "@m.ru"));
         }
         return result;
     }
