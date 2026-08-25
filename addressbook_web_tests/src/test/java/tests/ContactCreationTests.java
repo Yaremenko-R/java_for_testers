baf775f3225d4c57105670840d92519076aa5a23
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-25 17:14:49 +0300

    в создание контактов добавлено сравнение списков

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -6,15 +6,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     public static List<ContactData> contactProvider() {
         var result = new ArrayList<ContactData>();
-        for (var firstname : List.of("", "contact name")) {
-            for (var middlename : List.of("", "contact header")) {
-                for (var lastname : List.of("", "contact footer")) {
+        for (var firstname : List.of("", "contact firstname")) {
+            for (var middlename : List.of("", "contact middlename")) {
+                for (var lastname : List.of("", "contact lastname")) {
                     result.add(new ContactData()
                             .withFirstname(firstname)
                             .withMiddlename(middlename)
@@ -40,10 +41,18 @@ public class ContactCreationTests extends TestBase {
     @ParameterizedTest
     @MethodSource("contactProvider")
     public void canCreateMultipleContacts(ContactData contact) {
-        int contactCount = app.contacts().getCount();
+        var oldContacts = app.contacts().getList();
         app.contacts().createContact(contact);
-        int newContactCount = app.contacts().getCount();
-        Assertions.assertEquals(contactCount + 1, newContactCount);
+        var newContacts = app.contacts().getList();
+        Comparator<ContactData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newContacts.sort(compareById);
+        var expectedList = new ArrayList<>(oldContacts);
+        expectedList.add(contact.withId(newContacts.get(newContacts.size() - 1).id()).withFirstname("").withMiddlename("").withLastname("")
+                .withAddress("").withMobile("").withEmail(""));
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newContacts, expectedList);
     }
 
 }
