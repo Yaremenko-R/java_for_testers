16626f16f3a14093f74930343025a53bd0364ee8
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-29 15:20:18 +0300

    добавлено свойство photo

--- addressbook_web_tests/src/test/java/model/ContactData.java
+++ addressbook_web_tests/src/test/java/model/ContactData.java
@@ -1,38 +1,41 @@
 package model;
 
 public record ContactData(String id, String firstname, String middlename, String lastname, String address,
-                          String mobile,
-                          String email) {
+                          String mobile, String email, String photo) {
     public ContactData() {
-        this("", "", "", "", "", "", "");
+        this("", "", "", "", "", "", "", "");
     }
 
     public ContactData withId(String id) {
-        return new ContactData(id, firstname, this.middlename, this.lastname, this.address, this.mobile, this.email);
+        return new ContactData(id, firstname, this.middlename, this.lastname, this.address, this.mobile, this.email, this.photo);
     }
 
     public ContactData withFirstname(String firstname) {
-        return new ContactData(this.id, firstname, this.middlename, this.lastname, this.address, this.mobile, this.email);
+        return new ContactData(this.id, firstname, this.middlename, this.lastname, this.address, this.mobile, this.email, this.photo);
     }
 
     public ContactData withMiddlename(String middlename) {
-        return new ContactData(this.id, this.firstname, middlename, this.lastname, this.address, this.mobile, this.email);
+        return new ContactData(this.id, this.firstname, middlename, this.lastname, this.address, this.mobile, this.email, this.photo);
     }
 
     public ContactData withLastname(String lastname) {
-        return new ContactData(this.id, this.firstname, this.middlename, lastname, this.address, this.mobile, this.email);
+        return new ContactData(this.id, this.firstname, this.middlename, lastname, this.address, this.mobile, this.email, this.photo);
     }
 
     public ContactData withAddress(String address) {
-        return new ContactData(this.id, this.firstname, this.middlename, this.lastname, address, this.mobile, this.email);
+        return new ContactData(this.id, this.firstname, this.middlename, this.lastname, address, this.mobile, this.email, this.photo);
     }
 
     public ContactData withMobile(String mobile) {
-        return new ContactData(this.id, this.firstname, this.middlename, this.lastname, this.address, mobile, this.email);
+        return new ContactData(this.id, this.firstname, this.middlename, this.lastname, this.address, mobile, this.email, this.photo);
     }
 
     public ContactData withEmail(String email) {
-        return new ContactData(this.id, this.firstname, this.middlename, this.lastname, this.address, this.mobile, email);
+        return new ContactData(this.id, this.firstname, this.middlename, this.lastname, this.address, this.mobile, email, this.photo);
+    }
+
+    public ContactData withPhoto(String photo) {
+        return new ContactData(this.id, this.firstname, this.middlename, this.lastname, this.address, this.mobile, this.email, photo);
     }
 }
 
