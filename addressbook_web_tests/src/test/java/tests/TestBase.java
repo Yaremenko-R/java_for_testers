d5527e2e09cefbe080eb6c87882f39b7d58b1594
Yaremenko-R <roman.jaremenko@gmail.com>
2026-08-31 16:13:29 +0300

    перенос randomFile из TestBase в CommonFunctions

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,10 +3,6 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.io.File;
-import java.nio.file.Paths;
-import java.util.Random;
-
 public class TestBase {
 
     protected static ApplicationManager app;
@@ -17,13 +13,6 @@ public class TestBase {
             app = new ApplicationManager();
         }
         app.init(System.getProperty("browser", "firefox"));
-    }
-
-    public static String randomFile(String dir) {
-        var fileNames = new File(dir).list();
-        var rnd = new Random();
-        var index = rnd.nextInt(fileNames.length);
-        return Paths.get(dir, fileNames[index]).toString();
     }
 
 }
