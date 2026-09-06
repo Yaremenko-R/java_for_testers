7255a93330198619d822b3c88a2ed158fd0daaaf
Yaremenko-R <roman.jaremenko@gmail.com>
2026-09-06 20:32:29 +0300

    проверки перенесены на уровень бд

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -19,13 +19,6 @@ public class GroupCreationTests extends TestBase {
 
     public static List<GroupData> groupProvider() throws IOException {
         var result = new ArrayList<GroupData>();
-//        for (var name : List.of("", "group name")) {
-//            for (var header : List.of("", "group header")) {
-//                for (var footer : List.of("", "group footer")) {
-//                    result.add(new GroupData().withName(name).withHeader(header).withFooter(footer));
-//                }
-//            }
-//        }
         var json = "";
         try (var reader = new FileReader("groups.json");
              var breader = new BufferedReader(reader)
@@ -47,15 +40,16 @@ public class GroupCreationTests extends TestBase {
     @ParameterizedTest
     @MethodSource("groupProvider")
     public void canCreateMultipleGroups(GroupData group) {
-        var oldGroups = app.groups().getList();
+        var oldGroups = app.jdbc().getGroupList();
         app.groups().createGroup(group);
-        var newGroups = app.groups().getList();
+        var newGroups = app.jdbc().getGroupList();
         Comparator<GroupData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
         newGroups.sort(compareById);
+        var maxId = newGroups.get(newGroups.size() - 1).id();
         var expectedList = new ArrayList<>(oldGroups);
-        expectedList.add(group.withId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
+        expectedList.add(group.withId(maxId));
         expectedList.sort(compareById);
         Assertions.assertEquals(newGroups, expectedList);
     }
