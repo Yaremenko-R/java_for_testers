0e3ce7df317c83b8f6cd94e070d405a3822cd934
Yaremenko-R <roman.jaremenko@gmail.com>
2026-09-07 00:57:38 +0300

    создан вспомогательный класс ContactRecord

---
+++ addressbook_web_tests/src/test/java/manager/hbm/ContactRecord.java
@@ -0,0 +1,37 @@
+package manager.hbm;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "addressbook")
+public class ContactRecord {
+
+    @Id
+    @Column(name = "id")
+    public int id;
+
+    @Column(name = "firstname")
+    public String firstname;
+
+    @Column(name = "middlename")
+    public String middlename;
+
+    @Column(name = "lastname")
+    public String lastname;
+
+    @Column(name = "address")
+    public String address;
+
+    @Column(name = "mobile")
+    public String mobile;
+
+    @Column(name = "email")
+    public String email;
+
+    @Column(name = "photo")
+    public String photo;
+
+}
