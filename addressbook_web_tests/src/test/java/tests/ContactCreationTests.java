62547dc920c0c9b4453057f347f307f2576f6f11
Yaremenko-R <roman.jaremenko@gmail.com>
2026-09-09 00:48:08 +0300

    создаем контакт и включаем в группу

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -3,7 +3,9 @@ package tests;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.ContactData;
+import model.GroupData;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -65,6 +67,20 @@ public class ContactCreationTests extends TestBase {
         expectedList.add(contact.withId(maxId).withPhoto(""));
         expectedList.sort(compareById);
         Assertions.assertEquals(newContacts, expectedList);
+    }
+
+    @Test
+    public void canCreateContactInGroup() {
+        var contact = new ContactData().withFirstname("Ivan").withMiddlename("Ivanych")
+                        .withLastname("Ivanov").withAddress("Москва").withMobile("+7(495)577-05-13").withEmail("q@m.ru");
+        if (app.hbm().getGroupCount() == 0) {
+            app.hbm().createGroup(new GroupData().withName("group name").withHeader("group header").withFooter( "group footer"));
+        }
+        var group = app.hbm().getGroupList().get(0);
+        var oldRelated = app.hbm().getContactsInGroup(group);
+        app.contacts().createContact(contact, group);
+        var newRelated = app.hbm().getContactsInGroup(group);
+        Assertions.assertEquals(oldRelated.size() + 1, newRelated.size());
     }
 
 }
