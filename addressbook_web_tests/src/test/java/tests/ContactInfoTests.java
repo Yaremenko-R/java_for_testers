02c342fabb9f556ea8e65f168d51b72a99d753fa
Yaremenko-R <roman.jaremenko@gmail.com>
2026-09-17 23:19:37 +0300

    разделили тест на отдельные проверки телефоны, email, адреса

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -17,14 +17,27 @@ public class ContactInfoTests extends TestBase {
                             .withLastname("Ivanov").withAddress("Москва").withMobile("+7(495)577-05-13").withEmail("q@m.ru"));
         }
         var contacts = app.hbm().getContactList();
-        var expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.home(), contact.mobile(), contact.work(), contact.email()
-                                , contact.email2(), contact.email3(), contact.address())
+        var expectedPhones = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.home(), contact.mobile(), contact.work())
                         .filter(s -> s != null && !"".equals(s))
                         .collect(Collectors.joining("\n"))
         ));
-        var allInOne = app.contacts().getAllContactInfo();
-        Assertions.assertEquals(expected, allInOne);
+        var expectedEmails = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.email(), contact.email2(), contact.email3())
+                        .filter(s -> s != null && !"".equals(s))
+                        .collect(Collectors.joining("\n"))
+        ));
+        var expectedAddresses = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.address())
+                        .filter(s -> s != null && !"".equals(s))
+                        .collect(Collectors.joining("\n"))
+        ));
+        var allPhones = app.contacts().getPhones();
+        var allEmails = app.contacts().getEmails();
+        var allAdresses = app.contacts().getAddresses();
+        Assertions.assertEquals(expectedPhones, allPhones);
+        Assertions.assertEquals(expectedEmails, allEmails);
+        Assertions.assertEquals(expectedAddresses, allAdresses);
     }
 
 }
